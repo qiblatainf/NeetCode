3a4f63715290c316ce73d34375a317ac4ae6d8ec
Qiblatain Fatima <63148120+qiblatainf@users.noreply.github.com>
2023-05-15 20:29:30 +0500

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,28 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int sum = 0;
+        int num1 = 0;
+        int num2 = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i+1; j < nums.length; j++){
+                if (i != j){
+                    sum = nums[i] + nums[j];
+                    // System.out.println(sum);
+                    if (sum == target){
+                        num1 = i;
+                        num2 = j;
+                        break;
+                    }
+                    else{
+                        sum = 0;
+                    }
+                }
+
+            }
+        }
+        int [] output = new int[]{num1, num2};
+        return output ;
+    }
+}
