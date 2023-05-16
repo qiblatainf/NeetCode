18ef352b01808b0246578bca1236075da51667a1
Qiblatain Fatima <63148120+qiblatainf@users.noreply.github.com>
2023-05-17 02:57:22 +0500

    Two Pointer Approach

--- ValidPalindrome2P.java
+++ ValidPalindrome2P.java
@@ -2,7 +2,6 @@ class ValidPalindrome {
     public boolean isPalindrome(String s) {
 
         s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
         boolean ispalindrome = true;
 
         for (int i = 0; i < s.length()/2; i++){
