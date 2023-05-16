451bba375c815cbdb151167460e8ad124410dffa
Qiblatain Fatima <63148120+qiblatainf@users.noreply.github.com>
2023-05-17 02:22:50 +0500

    Create ValidPalindrome.java

---
+++ ValidPalindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        String reverse = "";
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase();
+
+        char [] characters = s.toCharArray();
+
+        for (char c: characters){
+            reverse = c + "" + reverse;
+        }
+
+        System.out.println(reverse);
+
+        if (!reverse.equals(s)) return false;
+        return true;
+        
+    }
+}
